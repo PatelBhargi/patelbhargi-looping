35b380a5c050180d6c4cb5774a3acafa4b54d921
PatelBhargi <133199263+PatelBhargi@users.noreply.github.com>
2023-05-23 12:25:18 +0530

    Add files via upload

---
+++ programe7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+main()
+{
+	int y;
+	printf("enter any year:");
+	scanf("%d",&y);
+	if(y%400==0||y%4==0 && y%100!=0)
+
+	{
+		printf("year is leapyear");
+		
+	}
+	else
+	
+	{
+		printf("year is not leapyear");
+	}
+	return 0;
+}
